6e14eb5817d6730933d47b1391e50e493a8c807b
swati07010 <swati030107@gmail.com>
2024-10-03 00:21:19 +0530

    This is the frontend of an eCommerce website

---
+++ frontend/src/Pages/LoginSignup.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./CSS/LoginSignup.css";
+function LoginSignup() {
+  return (
+    <div className="loginsignup">
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <diV className="loginsignup-fields">
+          <input type="text" placeholder="Your name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="password" />
+        </diV>
+        <button>Continue</button>
+        <p className="loginsignup-login">
+          Already have an accounts? <span>Login here</span>
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continuing ,I agree to the terms of use & privacy policy</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LoginSignup;
